72ac2dbe06192266c35c4ea7a6c96c95873ccc77
Devinder <devinderwadhwa20@gmail.com>
2026-08-31 19:56:53 +0530

    Sequence updated

--- bharatcart-backend/src/main/java/com/bharatcart/bharatcart_backend/config/SecurityConfig.java
+++ bharatcart-backend/src/main/java/com/bharatcart/bharatcart_backend/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,7 +35,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(basic -> basic.disable());
 
         return http.build();
     }
